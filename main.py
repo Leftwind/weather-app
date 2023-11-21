43bb900f067644d7e927d451fff841606758315b
Leftwind <andreselfisico@gmail.com>
2023-11-21 17:54:58 +0100

    Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+#Website Object
+app = Flask("Website")
+
+@app.route("/home/")
+def home():
+    return render_template("tutorial.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+app.run(debug=True)
