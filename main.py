646f73f069e8d43965c6e7c2ecefe6dbca55fa61
Leftwind <andreselfisico@gmail.com>
2023-11-27 18:25:23 +0100

    Completec with final functioon

--- main.py
+++ main.py
@@ -23,9 +23,31 @@ def about(station, date):
     df = pd.read_csv(filename, skiprows = 20, parse_dates=['    DATE'])
        
     temperature = df.loc[df['    DATE']==date]['   TG'].squeeze() /10 
-    return {"statioon": station,
+    return {"station": station,
             "date": date,
             "temperature": temperature}
+
+@app.route("/api/v1/<station>/")
+def about2(station):
+    #We want all the data from 1 station
+    filename = 'data/TG_STAID' + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows =20, parse_dates=['    DATE'])
+
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/annual/<station>/<year>")
+def about3(station, year):
+
+    #We only need the temperature for the dic, the rest will be asked to the user. 
+    filename = 'data/TG_STAID' + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows = 20)
+    # DAte column to string
+    df["    DATE"] = df["    DATE"].astype(str)
+
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient="records")
+
+    return result
 
 if __name__ == '__main__':
    app.run(debug=True, port=5001)
